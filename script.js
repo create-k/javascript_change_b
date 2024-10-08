c107f83db6a84b99fc7cc03be76a074371ddb26a
create-k <131327436+create-k@users.noreply.github.com>
2024-10-08 10:39:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// JavaScript
+
+const btnA = document.querySelector('#btn-a');
+const btnB = document.querySelector('#btn-b');
+const btnC = document.querySelector('#btn-c');
+const btnD = document.querySelector('#btn-d');
+const btnE = document.querySelector('#btn-e');
+const btnF = document.querySelector('#btn-f');
+const btnG = document.querySelector('#btn-g');
+const btnH = document.querySelector('#btn-h');
+const btnI = document.querySelector('#btn-i');
+const btnJ = document.querySelector('#btn-j');
+const btnK = document.querySelector('#btn-k');
+
+btnA.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-a');
+});
+
+btnB.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-b');
+});
+
+btnC.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-c');
+});
+
+btnD.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-d');
+});
+
+btnE.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-e');
+});
+
+btnF.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-f');
+});
+
+btnG.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-g');
+});
+
+btnH.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-h');
+});
+
+btnI.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-i');
+});
+
+btnJ.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-j');
+});
+
+btnK.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'color-k');
+});
